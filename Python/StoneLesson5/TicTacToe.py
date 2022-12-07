434becd7aab317a416c2e219f27e5e9a56fba437
MariMax <m9036827733@yandex.ru>
2022-12-07 14:37:42 +0300

    Программа "Крестики - Нолики". В процессе создания.

--- Python/StoneLesson5/TicTacToe.py
+++ Python/StoneLesson5/TicTacToe.py
@@ -5,37 +5,78 @@ print('Условия игры: Один из игроков играет за "
       'Удачи!')
 
 
-def print_field():
-      print()
-      print(f'\t|Ход игрока {player}|')
-      print('\t|     |     |     |')
-      print(f"\t|  {pl_fi[0]}  |  {pl_fi[1]}  |  {pl_fi[2]}  |")
-      print('\t|__1__|__2__|__3__|')
-      print('\t|     |     |     |')
-      print(f"\t|  {pl_fi[3]}  |  {pl_fi[4]}  |  {pl_fi[5]}  |")
-      print('\t|__4__|__5__|__6__|')
-      print('\t|     |     |     |')
-      print(f"\t|  {pl_fi[6]}  |  {pl_fi[7]}  |  {pl_fi[8]}  |")
-      print('\t|__7__|__8__|__9__|')
-      print()
+def print_field(pl_fi):
+    print()
+    print(f'\t _________________')
+    print('\t|     |     |     |')
+    print(f"\t|  {pl_fi[0]}  |  {pl_fi[1]}  |  {pl_fi[2]}  |")
+    print('\t|_____|_____|_____|')
+    print('\t|     |     |     |')
+    print(f"\t|  {pl_fi[3]}  |  {pl_fi[4]}  |  {pl_fi[5]}  |")
+    print('\t|_____|_____|_____|')
+    print('\t|     |     |     |')
+    print(f"\t|  {pl_fi[6]}  |  {pl_fi[7]}  |  {pl_fi[8]}  |")
+    print('\t|_____|_____|_____|')
+    print()
 
-pl_fi = [' ' for i in range (1,10)]
 
-###########################################
-player1 = 'Максим' #input('Введите имя игрока: ')
-player2 = 'Марианна' #input('Введите имя игрока: ')
-player = player1
+def player_sign(player_sign):
+    valid = False
+    while not valid:
+        take = input("Куда поставим " + player_sign + "? >>> ")
+        try:
+            take = int(take)
+        except:
+            print("Вы ввели не число! Повторите ввод:")
+            continue
+        if take >= 1 and take <= 9:
+            if (str(pl_fi[take - 1]) not in "XO"):
+                pl_fi[take - 1] = player_sign
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
 
-print_field()
 
-def selected_field():
-      select = int(input('    Я выбираю поле: '))
-      if player == player1:
-            pl_fi[select-1] =  'X'
-            player = player2
-      else:
-            pl_fi[select - 1] = 'O'
-            player = player1
+def check_result(pl_fi):
+    win_results = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for i in win_results:
+        if pl_fi[i[0]] == pl_fi[i[1]] == pl_fi[i[2]]:
+            return pl_fi[i[0]]
+    return False
 
+
+pl_fi = [i for i in range(1, 10)]
+
+
+player1 = 'Максим'
+player2 = 'Марианна'
+player = ''
+
+
+i = 0
+win = False
+while not win:
+    print_field(pl_fi)
+    if i % 2 == 0:
+        player = player1
+        print(f'Ход игрока {player}')
+        player_sign("X")
+    else:
+        player = player2
+        print(f'Ход игрока {player}')
+        player_sign("O")
+    i += 1
+    if i > 4:
+        temp = check_result(pl_fi)
+        if temp:
+            print_field(pl_fi)
+            print(f'Игрок {player} Выиграл!')
+            win = True
+            break
+    if i == 9:
+        print("Свободных клеток не осталось. Ничья!")
+        break
 
 
