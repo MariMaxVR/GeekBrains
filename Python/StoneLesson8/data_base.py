d87b1393a3335909acbad8749ae997f08d4ca5b8
MariMaxVR <m9036827733@yandex.ru>
2022-12-16 11:47:54 +0300

    Работа над телефонной книгой.

---
+++ Python/StoneLesson8/data_base.py
@@ -0,0 +1,24 @@
+import phone_book
+
+def load_phone_book():
+    with open('phone_book.txt', 'r', encoding='UTF-8') as file:
+        phone_book = file.readlines()
+    phone_book.set_phone_book(str_to_list(phone_book))
+
+def str_to_list(phone_book: list):
+    new_phone_book = []
+    for i in phone_book:
+        new_phone_book.append(i.strip().split(';'))
+    return new_phone_book
+
+def save_phone_book():
+    with open('phone_book.txt', 'w', encoding='UTF-8') as file:
+        file.write(list_to_str())
+
+def list_to_str():
+    phone_book_list = phone_book.get_phone_book()
+    new_phone_book = []
+    for i in phone_book_list:
+        new_phone_book.append(';'.join(i) + '\n')
+    new_phone_book[-1] = new_phone_book[-1][:-1]
+    return ''.join(new_phone_book)
---
+++ Python/StoneLesson8/menu.py
@@ -0,0 +1,43 @@
+import view
+import phone_book as PB
+import data_base as DB
+
+
+def main_menu(choice: int):
+    match choice:
+        case 1:
+            phone_book = PB.get_phone_book()
+            view.print_phone_book(phone_book)
+        case 2:
+            DB.load_phone_book()
+            view.load_successful()
+        case 3:
+            DB.save_phone_book()
+            view.save_successful()
+        case 4:
+            contact = view.new_contact()
+            PB.add_contact((contact))
+        case 5:
+            pass
+        case 6:
+            phone_book = PB.get_phone_book()
+            view.print_phone_book()
+            id = view.remove_choice()
+            if PB.remove_contact(id):
+                view.remove_successful()
+        case 7:
+            pass
+        case 0:
+            view.exit()
+            return True
+
+
+
+
+
+
+def start():
+    while True:
+        choice = view.show_menu()
+        if main_menu(choice):
+            break
