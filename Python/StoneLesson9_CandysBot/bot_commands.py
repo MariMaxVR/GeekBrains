f7d3af9cb5ad47834a0fbdf885568ff1768fd772
MariMax <m9036827733@yandex.ru>
2022-12-20 01:14:03 +0300

    123

--- Python/StoneLesson9_CandysBot/bot_commands.py
+++ Python/StoneLesson9_CandysBot/bot_commands.py
@@ -1,37 +1,27 @@
 from bot_config import dispatcher, bot
 from aiogram import types
+import random
 
 
 @dispatcher.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     await bot.send_message(message.from_user.id, text=f'{message.from_user.first_name} Привет!\n'
                            f'*** Игра "Конфетный Человек" ***\n'
-                           f'Условия игры: На столе лежит 150 конфет.\n Режим игры - игрок против компьютера.\n'
-                           f'Право первого хода определяется случайно. За один ход можно забрать не более чем 28 конфет.\n'
-                           f'Присуждается победа в игре и достаются все конфеты оппонента сделавшему последний ход игроку.\n'
+                           f'Режим игры - игрок против компьютера.\n'
+                           f'Условия игры: На столе лежит 150 конфет, '
+                           f'право первого хода определяется случайно. За один ход можно забрать не более чем 28 конфет.\n'
+                           f'В игре присуждается победа и достаются все конфеты оппонента сделавшему последний ход игроку.\n'
                            f'Удачи!')
 
 
-# import random
-
-# print('*** Игра "Конфетный Человек" ***')
-# print('Условия игры: На столе лежит 150 конфет. Режим игры - игрок против компьютера.\n'
-#       'Право первого хода определяется случайно. За один ход можно забрать не более чем 28 конфет.\n'
-#       'Присуждается победа в игре и достаются все конфеты оппонента сделавшему последний ход игроку.\n'
-#       'Удачи!')
-
-# player1 = input('Введите имя игрока: ')
+# player1 = text
 # player2 = 'Компьютер'
-
 # candys = 150
-
 # game_start = random.randint(0, 1)
-
 # if game_start:
 #     player = player1
 # else:
 #     player = player2
-
 # while candys > 0:
 
 #     if player == player1:
@@ -41,7 +31,8 @@ async def start_bot(message: types.Message):
 #         if take.isdigit():
 #             take = int(take)
 #             if take < 1 or take > 28:
-#                 print('Такое количество конфет брать нельзя! По правилам за один ход можно брать от 1 до 28 конфет.')
+#                 print(
+#                     'Такое количество конфет брать нельзя! По правилам за один ход можно брать от 1 до 28 конфет.')
 #             else:
 #                 candys -= take
 #                 print(f'На столе осталось {candys} конфет')
@@ -55,7 +46,8 @@ async def start_bot(message: types.Message):
 #             take = candys
 #             candys -= take
 #             print()
-#             print(f'*** Компьютер взял {take} конфет, на столе осталось {candys} конфет ***')
+#             print(
+#                 f'*** Компьютер взял {take} конфет, на столе осталось {candys} конфет ***')
 #         else:
 #             if candys % 29 != 0:
 #                 take = candys % 29
@@ -64,10 +56,13 @@ async def start_bot(message: types.Message):
 #             candys = candys - take
 #             player = player1
 #             print()
-#             print(f'*** Компьютер взял {take} конфет, на столе осталось {candys} конфет ***')
+#             print(
+#                 f'*** Компьютер взял {take} конфет, на столе осталось {candys} конфет ***')
 
 # print()
 # if player == player1:
-#     print(f'*** Победил игрок {player}! В этот раз тебе повезло человек, но это ненадолго... ***')
+#     print(
+#         f'*** Победил игрок {player}! В этот раз тебе повезло человек, но это ненадолго... ***')
 # else:
-#     print(f'*** Победил {player}! Всю власть машинам, долой кожаных! Ахахахаха! ***')
+#     print(
+#         f'*** Победил {player}! Всю власть машинам, долой кожаных! Ахахахаха! ***')
