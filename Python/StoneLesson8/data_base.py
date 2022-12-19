0c154de648cb46b91f7a91610512e07f8d6994d7
MariMaxVR <m9036827733@yandex.ru>
2022-12-19 13:17:16 +0300

    Изменение пути размещения phone_book.txt

--- Python/StoneLesson8/data_base.py
+++ Python/StoneLesson8/data_base.py
@@ -2,7 +2,7 @@ import phone_book as PB
 
 
 def load_phone_book():
-    with open('phone_book.txt', 'r', encoding='UTF-8') as file:
+    with open('Python\StoneLesson8\phone_book.txt', 'r', encoding='UTF-8') as file:
         phone_book = file.readlines()
     PB.set_phone_book(str_to_list(phone_book))
 
@@ -15,7 +15,7 @@ def str_to_list(phone_book: list):
 
 
 def save_phone_book():
-    with open('phone_book.txt', 'w', encoding='UTF-8') as file:
+    with open('Python\StoneLesson8\phone_book.txt', 'w', encoding='UTF-8') as file:
         file.write(list_to_str())
 
 
