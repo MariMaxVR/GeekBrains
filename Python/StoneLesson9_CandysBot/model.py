598106a9e7f9f3d29741d5fd975b8eee1046a4b5
MariMaxVR <m9036827733@yandex.ru>
2022-12-22 12:27:00 +0300

    Добавление возможности выбора сложности игры

--- Python/StoneLesson9_CandysBot/model.py
+++ Python/StoneLesson9_CandysBot/model.py
@@ -3,18 +3,20 @@ import random
 game = False
 player_name = ''
 total_candys = 150
-
+game_mode = 0
 
 async def set_game():
     global game
     global player_name
     global total_candys
+    global game_mode
     if game == False:
         game = True
     else:
         game = False
         player_name = ''
         total_candys = 150
+        game_mode = 0
 
 
 async def set_player_name(name):
@@ -24,10 +26,17 @@ async def set_player_name(name):
 
 async def bot_take():
     global total_candys
-    if total_candys % 29 != 0:
-        take = total_candys % 29
+    global game_mode
+    if game_mode == 1:
+        if total_candys % 29 != 0:
+            take = total_candys % 29
+        else:
+            take = random.randint(1, 28)
     else:
-        take = random.randint(1, 28)
+        if total_candys <= 28:
+            take = total_candys
+        else:
+            take = random.randint(1,28)
     return take
 
 
