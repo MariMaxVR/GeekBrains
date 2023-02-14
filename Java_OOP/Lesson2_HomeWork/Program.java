2c52d89b73ad3f45b9a8531ae0356737546d8b4b
MariMax <m9036827733@yandex.ru>
2023-02-14 09:28:00 +0300

    Lesson2_HomeWork_Update

--- Java_OOP/Lesson2_HomeWork/Program.java
+++ Java_OOP/Lesson2_HomeWork/Program.java
@@ -4,31 +4,31 @@ public class Program {
 
     public static void main(String[] args) {
 
-        VendingMachine vendine1 = new VendingMachine();
+        VendingMachine vm1 = new VendingMachine();
 
-        vendine1.putProduct("Snickers", 75.80, "Chocolate");
-        vendine1.putProduct("CocaCola", 89.50, "Soda");
-        vendine1.putProduct("Chernogolovka", 80.10, "Russian Soda");
-        vendine1.putProduct("Alyonka", 100, "Russian Chocolate");
+        vm1.putProduct("Snickers", 75.80, "Chocolate");
+        vm1.putProduct("CocaCola", 89.50, "Soda");
+        vm1.putProduct("Chernogolovka", 80.10, "Russian Soda");
+        vm1.putProduct("Alyonka", 100, "Russian Chocolate");
 
-        vendine1.getStatus();
+        vm1.getStatus();
         System.out.println();
 
-        vendine1.getProduct("CocaCola");
+        vm1.getProduct("CocaCola");
 
         System.out.println();
-        vendine1.getStatus();
+        vm1.getStatus();
 
-        vendine1.putProduct("CocaCola", 93.50, "Soda");
-        vendine1.putProduct("CocaCola", 89.50, "Soda");
-
-        System.out.println();
-        vendine1.getStatus();
-        System.out.println();
-
-        vendine1.getProduct("Alyonka");
+        vm1.putProduct("CocaCola", 93.50, "Soda");
+        vm1.putProduct("CocaCola", 89.50, "Soda");
 
         System.out.println();
-        vendine1.getStatus();
+        vm1.getStatus();
+        System.out.println();
+
+        vm1.getProduct("Alyonka");
+
+        System.out.println();
+        vm1.getStatus();
     }
 }
