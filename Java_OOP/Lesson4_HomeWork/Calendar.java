d345514f22fd456e9f0be9953c65222a497a892c
MariMax <m9036827733@yandex.ru>
2023-02-21 22:07:38 +0300

    Java_Lesson4

--- Java_OOP/Lesson4_HomeWork/Calendar.java
+++ Java_OOP/Lesson4_HomeWork/Calendar.java
@@ -1,30 +1,30 @@
 package Lesson4_HomeWork;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class Calendar {
 
     private static List<BackendTasks> listOfBackendTasks = new ArrayList<>();
-    private static List<FrontendTasks> listOfFrontendTasks = new ArrayList<>();
+    static List<FrontendTasks> listOfFrontendTasks = new ArrayList<>();
 
     public static void getListOfBackendTasks() {
         System.out.println(listOfBackendTasks);
 
     }
 
-    public void addToListOfBackendTasks(BackendTasks object) {
-        listOfBackendTasks.add(object);
-
+    public static void addToListOfBackendTasks(BackendTasks object) {
+        Calendar.listOfBackendTasks.add(object);
     }
 
     public static void getListOfFrontendTasks() {
-        System.out.println(listOfBackendTasks);
+        System.out.println(listOfFrontendTasks);
 
     }
 
     public static void addToListOfFrontendTasks(FrontendTasks object) {
-        listOfFrontendTasks.add(object);
+        Calendar.listOfFrontendTasks.add(object);
     }
 
 }
--- Java_OOP/Lesson4_HomeWork/Program.java
+++ Java_OOP/Lesson4_HomeWork/Program.java
@@ -5,54 +5,38 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        FrontendTasks f1 = new FrontendTasks(1, "Test1", "Test2", "Test3");
+        Scanner scanner = new Scanner(System.in);
 
-        // System.out.println(f1); Так распечатывает нормально.
+        while (true) {
 
-        Calendar.addToListOfFrontendTasks(f1);
-        Calendar.getListOfFrontendTasks();
+            System.out.print("Какого типа задачу будем добавлять?, \n" +
+                    "1 - Создание задачи для отдела Frontend. \n" +
+                    "2 - Создание задачи для отдела Backend. \n" +
+                    "3 - Просмотр задач отдела Frontend. \n" +
+                    "4 - Просмотр задач отдела Backend. \n" +
+                    "0 - Выход. \n Введите значение: ");
 
-        //а так пустой список, где-то ошибка в добавлении.
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Добавляем задачу для отдела Frontend.");
+                FrontendTasks f = new FrontendTasks(0, null, null, null);
+                System.out.print("Введите приоритет задачи: ");
 
+                f.setPriority(scanner.nextInt());
+                System.out.print("Введите автора задачи: ");
+                f.setAuthor(scanner.next());
+                System.out.print("Введите тему задачи: ");
+                f.setTaskTheme(scanner.next());
+                System.out.println("Введите deadline задачи: ");
+                f.setDeadLine(scanner.next());
+                System.out.println(f);
 
+            } else {
+                scanner.close();
+                break;
+            }
 
-
-        // Scanner scanner = new Scanner(System.in);
-
-        // while (true) {
-
-        //     System.out.print("Какого типа задачу будем добавлять?, \n" +
-        //             "1 - Создание задачи для отдела Frontend. \n" +
-        //             "2 - Создание задачи для отдела Backend. \n" +
-        //             "3 - Просмотр задач отдела Frontend. \n" +
-        //             "4 - Просмотр задач отдела Backend. \n" +
-        //             "0 - Выход. \n Введите значение: ");
-
-        //     int choice = scanner.nextInt();
-        //     if (choice == 0) {
-        //         break;
-        //     } else if (choice == 1) {
-        //         System.out.println("Добавляем задачу для отдела Frontend.");
-        //         FrontendTasks f = new FrontendTasks(0, null, null, null);
-        //         System.out.print("Введите приоритет задачи: ");
-        //         int prior = scanner.nextInt();
-        //         f.setPriority(prior);
-
-        //         System.out.print("Введите автора задачи: ");
-        //         f.setAuthor(scanner.nextLine());
-
-        //         System.out.print("Введите тему задачи: ");
-        //         f.setTaskTheme(scanner.nextLine());
-
-        //         System.out.print("Введите deadline задачи: ");
-        //         f.setDeadLine(scanner.nextLine());
-        //         Calendar.addToListOfFrontendTasks(f);
-
-        //         Calendar.getListOfFrontendTasks();
-
-        //     }
-
-        // }
+        }
 
     }
 
