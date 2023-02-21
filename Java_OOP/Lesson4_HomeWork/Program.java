24e1aab56450e2e24849437ce3e3ab89f6db7510
MariMax <m9036827733@yandex.ru>
2023-02-21 19:33:58 +0300

    Lesson4

--- Java_OOP/Lesson4_HomeWork/Program.java
+++ Java_OOP/Lesson4_HomeWork/Program.java
@@ -5,42 +5,54 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        FrontendTasks f1 = new FrontendTasks(1, "Test1", "Test2", "Test3");
 
-        while (true) {
+        // System.out.println(f1); Так распечатывает нормально.
 
-            System.out.print("Какого типа задачу будем добавлять?, \n" +
-                    "1 - Создание задачи для отдела Frontend. \n" +
-                    "2 - Создание задачи для отдела Backend. \n" +
-                    "3 - Просмотр задач отдела Frontend. \n" +
-                    "4 - Просмотр задач отдела Backend. \n" +
-                    "0 - Выход. \n Введите значение: ");
+        Calendar.addToListOfFrontendTasks(f1);
+        Calendar.getListOfFrontendTasks();
 
-            int choice = scanner.nextInt();
-            if (choice == 0) {
-                break;
-            } else if (choice == 1) {
-                System.out.println("Добавляем задачу для отдела Frontend.");
-                FrontendTasks f = new FrontendTasks(0, null, null, null);
-                System.out.print("Введите приоритет задачи: ");
-                int prior = scanner.nextInt();
-                f.setPriority(prior);
+        //а так пустой список, где-то ошибка в добавлении.
 
-                System.out.print("Введите автора задачи: ");
-                f.setAuthor(scanner.nextLine());
 
-                System.out.print("Введите тему задачи: ");
-                f.setTaskTheme(scanner.nextLine());
 
-                System.out.print("Введите deadline задачи: ");
-                f.setDeadLine(scanner.nextLine());
-                Calendar.addToListOfFrontendTasks(f);
 
-                Calendar.getListOfFrontendTasks();
+        // Scanner scanner = new Scanner(System.in);
 
-            }
+        // while (true) {
 
-        }
+        //     System.out.print("Какого типа задачу будем добавлять?, \n" +
+        //             "1 - Создание задачи для отдела Frontend. \n" +
+        //             "2 - Создание задачи для отдела Backend. \n" +
+        //             "3 - Просмотр задач отдела Frontend. \n" +
+        //             "4 - Просмотр задач отдела Backend. \n" +
+        //             "0 - Выход. \n Введите значение: ");
+
+        //     int choice = scanner.nextInt();
+        //     if (choice == 0) {
+        //         break;
+        //     } else if (choice == 1) {
+        //         System.out.println("Добавляем задачу для отдела Frontend.");
+        //         FrontendTasks f = new FrontendTasks(0, null, null, null);
+        //         System.out.print("Введите приоритет задачи: ");
+        //         int prior = scanner.nextInt();
+        //         f.setPriority(prior);
+
+        //         System.out.print("Введите автора задачи: ");
+        //         f.setAuthor(scanner.nextLine());
+
+        //         System.out.print("Введите тему задачи: ");
+        //         f.setTaskTheme(scanner.nextLine());
+
+        //         System.out.print("Введите deadline задачи: ");
+        //         f.setDeadLine(scanner.nextLine());
+        //         Calendar.addToListOfFrontendTasks(f);
+
+        //         Calendar.getListOfFrontendTasks();
+
+        //     }
+
+        // }
 
     }
 
