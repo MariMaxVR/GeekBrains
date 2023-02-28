c5da882f69ce3aaf3ddfaa2c5d34c2c10d8d98d2
MariMax <m9036827733@yandex.ru>
2023-02-28 16:45:12 +0300

    Lesson6_HW_Update

--- Java_OOP/Lesson6_HomeWork/WeatherConsole/src/main/java/ProjectWeatherConsole/UserInterfaceView.java
+++ Java_OOP/Lesson6_HomeWork/WeatherConsole/src/main/java/ProjectWeatherConsole/UserInterfaceView.java
@@ -7,26 +7,26 @@ public class UserInterfaceView {
     private Controller controller = new Controller();
 
     public void runInterface() {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Введите имя города: ");
+                String city = scanner.nextLine();
 
-        while (true) {
-            System.out.println("Введите имя города: ");
-            String city = scanner.nextLine();
+                System.out.println("Введите 1 для получения погоды на сегодня;" +
+                        "Введите 5 для прогноза на 5 дней; Для выхода введите 0:");
 
-            System.out.println("Введите 1 для получения погоды на сегодня;" +
-                    "Введите 5 для прогноза на 5 дней; Для выхода введите 0:");
+                String command = scanner.nextLine();
 
-            String command = scanner.nextLine();
+                // TODO* Сделать метод валидации пользовательского ввода
 
-            // TODO* Сделать метод валидации пользовательского ввода
+                if (command.equals("0"))
+                    break;
 
-            if (command.equals("0"))
-                break;
-
-            try {
-                controller.getWeather(command, city);
-            } catch (IOException e) {
-                e.printStackTrace();
+                try {
+                    controller.getWeather(command, city);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
