fb884c919ccada67b932e8794d5eb105c1dc8f65
MariMax <m9036827733@yandex.ru>
2023-03-03 10:05:30 +0300

    Update

--- Java_OOP/Lesson5_HomeWork/Task3/Client.java
+++ Java_OOP/Lesson5_HomeWork/Task3/Client.java
@@ -7,31 +7,29 @@ import java.util.Scanner;
 
 public class Client {
 
-    public class ClientApp {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            try (Socket socket = new Socket("localhost", 1234)) {
-                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                System.out.println("Выберите пункт меню:\n1 - добавить студента\n2 - удалить студента\n" +
-                        "3 - показать группу\n0 - выход.");
-                while (true) {
-                    String request = scanner.nextLine();
-                    dataOutputStream.writeUTF(request);
-                    if (request.equals("0"))
-                        break;
-
-                    System.out.println(dataInputStream.readUTF());
-                }
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            } finally {
-                scanner.close();
+        try (Socket socket = new Socket("localhost", 1234)) {
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Выберите пункт меню:\n1 - добавить студента\n2 - удалить студента\n" +
+                    "3 - показать группу\n0 - выход.");
+            while (true) {
+                String request = scanner.nextLine();
+                dataOutputStream.writeUTF(request);
+                if (request.equals("0"))
+                    break;
+                System.out.println(dataInputStream.readUTF());
             }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            scanner.close();
         }
     }
+
 }
